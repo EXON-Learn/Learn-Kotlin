dcc0a2003b38fe2e0bae86210d5a86fc23b28919
시혁 박 <exon.indiv@gmail.com>
2020-07-26 09:05:23 +0900

    반복문

--- app/app/src/main/java/com/example/kotlin/exam_script.js
+++
@@ -1,9 +0,0 @@
-var exam_script = {
-    plus_num: function(num){
-        try{
-            window.java.getNum(num+num);
-        }catch(err){
-            console.log(">> [exam_script.plus_num()] " + err);
-        }
-    }
-}
